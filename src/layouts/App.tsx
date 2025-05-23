cc9ddcf5bf58b6f91ee3682a4c1406cb739399e1
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-05-22 21:10:35 -0500

    fix: mejorar la estructura del componente App para una mejor presentación y organización del contenido

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -10,29 +10,18 @@ function App() {
         <div className="flex justify-center">
           <Carrusel />
         </div>
-        <div>    
-          <p className=""><span className="font-bold">MetaMercado</span> es un pagina creada para apoyar
-          a los emprendimientos en sus inicios, ayudando a los emprendedores a llegar al mercado digital.
-          En esta plataforma queremos dar apoyo directo a todo tipo de empresa que busca digitalizar su negocio.
-          </p>
+        <div className="flex flex-col items-center" >    
+          <h1 className="bg-blue-500 text-center font-bold text-2xl p-3 w-full">Descripcion</h1>
+          <p className="text-justify max-w-[700px] text-lg py-5"><span className="font-bold">MetaMercado</span> es un pagina creada para apoyar a los emprendimientos en sus inicios, ayudando a los emprendedores a llegar al mercado digital. En esta plataforma queremos dar apoyo directo a todo tipo de empresa que busca digitalizar su negocio.</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="bg-blue-950 text-white text-center font-bold text-2xl p-3 w-full">Video</h1>
+          <Carrusel />
+
         </div>
       </main>
-      <footer>
-        <div>
-          <p>
-            Creador uno
-          </p>
-        </div>
-        <div>
-          <p>
-            Creador dos
-          </p>
-        </div>
-        <div>
-          <p>
-            Creador tres
-          </p>
-        </div>
+      <footer className=" py-2">
+        <h1 className="bg-blue-300 text-center text-2xl font-bold p-3">Redes sociales</h1>
       </footer>
     </>
   )
